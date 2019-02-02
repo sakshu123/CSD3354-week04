c47fbf0a82bf62d962c9935c5206f7b784a68edb
visualstudio <visualstudio@122-PC35>
2019-02-01 20:03:17 -0500

    second commit

--- CSD3354-week04/Program.cs
+++ CSD3354-week04/Program.cs
@@ -10,6 +10,54 @@ namespace CSD3354_week04
     {
         static void Main(string[] args)
         {
+            //Countryside c = new Countryside();
+            //c.run();
+        }
+    }
+
+    class LearningExperiment
+    {
+        public void run()
+        {
+            Village Toronto;
+            Toronto = new Village();
+            Village a, b, c;
+            Toronto.villageName = "version A";
+            a = Toronto;
+            Console.WriteLine(a.villageName);
+            Toronto = new Village();
+            Toronto.villageName = "version B";
+            b = Toronto;
+            Console.WriteLine(b.villageName);
+            Toronto = new Village();
+            Toronto.villageName = "version C";
+            c = Toronto;
+            Console.WriteLine(c.villageName);
+
+
+        }
+    }
+
+
+    class Village
+    {
+        public bool isAstrilde;
+        public Village nextVillage;
+        public Village prevVillage;
+        public string villageName;
+    }
+    class Countryside
+    {
+        Village Toronto, Maple, Ajax;
+
+        public void run()
+        {
+            Maple = new Village();
+            Toronto = new Village();
+            Ajax = new Village();
+            Maple.villageName = "Toronto";
+            Maple.nextVillage = Toronto;
+
         }
     }
 }
